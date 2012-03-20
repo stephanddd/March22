74a1be10f7672866bcf81a3c230f1315e43306b9
Stephan D <setphand@gmail.com>
2012-03-20 03:32:55 -0400

    first commit

---
+++ March22/March22AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  March22AppDelegate.h
+//  March22
+//
+//  Created by Stephan Donelian on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface March22AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ March22/main.m
@@ -0,0 +1,27 @@
+//
+//  main.m
+//  March22
+//
+//  Created by Stephan Donelian on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "March22AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        int i = 10;
+        NSLog(@"The value of i is %d.", i);
+        
+        CGFloat f = 3.14159;
+        NSLog(@"The value of f is %g.", f);
+        
+        UIDevice *device = [UIDevice currentDevice];
+        NSString *version = device.systemVersion;
+        NSLog(@"Version of iOS: %@", version);	//Output an NString with %@.
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([March22AppDelegate class]));
+    }
+}
